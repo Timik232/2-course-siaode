69ab6e1d9a1f45ea70ee6ee19b43d30a913bd249
Timur <100406268+Timik232@users.noreply.github.com>
2022-09-08 15:03:55 +0300

    Update bits.cpp

--- bits.cpp
+++ bits.cpp
@@ -1,3 +1,4 @@
+
 //7 9 11
 //только с чётными номерами
 //128
@@ -8,101 +9,101 @@ using namespace std;
 
 void cout_result(unsigned short bit)
 {
-  unsigned short mask = 0b1000000000000000;
-  string output = "";
-  for (int i = 0; i <16; i++)
-  {
-    if (mask & bit)
-      output+="1";
-    else output+="0";
-    mask >>= 1;
-    
-  }
-  cout << output << endl;
+    unsigned short mask = 0b1000000000000000;
+    string output = "";
+    for (int i = 0; i < 16; i++)
+    {
+        if (mask & bit)
+            output += "1";
+        else output += "0";
+        mask >>= 1;
+
+    }
+    cout << output << endl;
 }
 void task1()
 {
-  unsigned short s = 0x0000;
-  usigned short mask = 0b0000101010000000;
-  s = s|mask;
-  cout << s << "     ";
-  cout_result(s);
+    unsigned short s = 0x0000;
+    unsigned short mask = 0b0000101010000000;
+    s = s | mask;
+    cout << s << "     ";
+    cout_result(s);
 }
 void task2()
 {
-  cout << "Enter hex-number\n";
-  unsigned short s;
-  cin >> hex >> s;
-  usigned short mask = 0b1010101010101010;
-  s = s & mask;
-  cout << s << "     ";
-  cout_result(s);
+    cout << "Enter hex-number\n";
+    unsigned short s;
+    cin >> hex >> s;
+    unsigned short mask = 0b1010101010101010;
+    s = s & mask;
+    cout << s << "     ";
+    cout_result(s);
 }
 void task3()
 {
- usigned short s;
- cout << "Enter hex-number\n";
- cin >> hex >> s;
- usigned short mask = 7;
- s <<= mask;
- cout << s << "     ";
- cout_result(s); 
+    unsigned short s;
+    cout << "Enter hex-number\n";
+    cin >> hex >> s;
+    unsigned short mask = 7;
+    s <<= mask;
+    cout << s << "     ";
+    cout_result(s);
 }
 void task4()
 {
- usigned short s;
- cout << "Enter hex-number\n";
- cin >> hex >> s;
- usigned short mask = 7;
- s >>= mask;
-  cout << s << "     ";
-  cout_result(s);
+    unsigned short s;
+    cout << "Enter hex-number\n";
+    cin >> hex >> s;
+    unsigned short mask = 7;
+    s >>= mask;
+    cout << s << "     ";
+    cout_result(s);
 }
 void task5()
 {
- usigned short s;
- cout << "Enter hex-number\n";
- cin >> hex >> s;
-  unsigned short mask = 0b0000000000000001;
-  cout << "Enter n: this bit will be 0\n";
-  unsigned short n;
-  cin >> n;
-  mask<<=n; 
-  //s = s | mask; //чтобы в 1
-  mask = ~mask;
-  s = s & mask; //чтобы в 0
-  cout << s << "     ";
-  cout_result(s);
+    unsigned short s;
+    cout << "Enter hex-number\n";
+    cin >> hex >> s;
+    unsigned short mask = 0b0000000000000001;
+    cout << "Enter n: this bit will be 0\n";
+    unsigned short n;
+    cin >> n;
+    mask <<= n;
+    //s = s | mask; //чтобы в 1
+    mask = ~mask;
+    s = s & mask; //чтобы в 0
+    cout << s << "     ";
+    cout_result(s);
 }
 
 int main()
 {
-cout << "Choose task (1-5) (or exit):\n";
-char t;
-cin >> t;
-while (true)
-{
-  switch (t)
+    while (true)
     {
-    case '1':
-      task1();
-      break;
-    case '2':
-      task2();
-      break;
-    case '3':
-      task3();
-      break;
-    case '4':
-      task4();
-      break;
-    case '5':
-      task5();
-      break;
-    case '6':
-     exit();
-    default:
-      cout << "Invalid input, try again\n";
+        cout << "Choose task (1-6) (or exit):\n 1.set 1 at 7, 9 and 11 bits\n 2.set 0 at even numbers\n 3.Multiply numbers\n4.Divide numbers\n5.Set to 0 bits\n6.Exit\n";
+        char t;
+        cin >> t;
+        switch (t)
+        {
+        case '1':
+            task1();
+            break;
+        case '2':
+            task2();
+            break;
+        case '3':
+            task3();
+            break;
+        case '4':
+            task4();
+            break;
+        case '5':
+            task5();
+            break;
+        case '6':
+            exit(0);
+        default:
+            cout << "Invalid input, try again\n";
+        }
     }
-  }
 }
