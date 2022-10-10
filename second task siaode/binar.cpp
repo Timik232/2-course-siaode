9d1410e7474cfc8ce7936c1ca2166dbfab4e9a48
Timur <100406268+Timik232@users.noreply.github.com>
2022-10-11 00:26:56 +0300

    almost second

--- second task siaode/binar.cpp
+++ second task siaode/binar.cpp
@@ -1,11 +1,11 @@
 ﻿#include "binary_module.cpp"
 int main()
 {
-    cout << "Enter name of file\n";
+    /*cout << "Enter name of file\n";
     string name;
     getline(cin, name);
-    create_bit(name);
-    cout << "Choose action with file(1-7):\n1.From text file to binary\n2.Print file\n3.From binary file to text\n4.Find element by index\n5.Common tests of all functions\n6.Personal task-test (12)\n7.Exit\n";
+    create_bit(name);*/
+    cout << "Choose action with file(1-7):\n1.From text file to binary (create binary file)\n2.Print file\n3.From binary file to text\n4.Find element by index\n5.Delete by key (license)\n6.Personal task-test (12)\n7.Exit\n";
     while (true)
     {
         char choose;
@@ -20,47 +20,73 @@ int main()
                 getline(cin, line);
                 buf += line;
             }
-            text_to_binar(buf, name);
-            cout << "Data from text file was added\n";
+            cout << "Enter name of binary file\n";
+            string name;
+            while (name == "")
+                getline(cin, name);
+            if (text_to_binar(buf, name))
+                cout << "Data from text file was added\n";
             cin.clear();
         }
         else if (choose == '2')
         {
+            cout << "Enter the name of binary file\n";
+            string name = "";
+            while (name == "")
+                getline(cin, name);
             print_file(name);
         }
         else if (choose == '3')
         {
             string line = "";
             string buf = "";
-            cout << "Write a name of file from which we will take data\n";
+            cout << "Write a name of text file to which we will write binary\n";
             while (line == "")
             {
                 getline(cin, line);
                 buf += line;
             }
-            binar_to_text(buf, name);
-            cout << "Data from binary file was added to text\n";
+            cout << "Enter name of binary file\n";
+            string name = "";
+            while (name == "")
+                getline(cin, name);
+            if (binar_to_text(buf, name))
+                cout << "Data from binary file was added to text\n";
             cin.clear();
         }
         else if (choose == '4')
         {
+            cout << "Enter name of binary file\n";
+            string name = "";
+            while (name == "")
+                getline(cin, name);
             cout << "Enter number's index\n";
             int n;
-            cin >> n;
-            //cout << "Number with index " << n << " is " << findNumber(name, n) << endl;
+            cin >> n;            
+            cout << "Name of business with index " << n << " is " << get_by_index(name, n).name << endl;
         }
         else if (choose == '5')
         {
-
-            print_file(name);
-            // add_line(name, "1 2 3 4 5");
-            print_file(name);
-            //cout << "Number with index 3 is " << findNumber(name, 3) << endl;
-           // cout << "There is " << numbersAmount(name) << " numbers in the file\n";
+            cout << "Enter name of binary file\n";
+            string name = "";
+            while (name == "")
+                getline(cin, name);
+            cout << "Print license of business, which you want to delete\n";
+            string key = "";
+            getline(cin, key);
+            if (delete_by_key(name, key))
+                cout << "Was replaced by the last element\n";
         }
         else if (choose == '6')
         {
-            //new_file(name);
+            cout << "Enter name of binary file\n";
+            string name = "";
+            while (name == "")
+                getline(cin, name);
+            cout << "Enter name of founder\n";
+            string founder;
+            getline(cin, founder);
+            founder_list(name, founder);
         }
         else if (choose == '7')
         {
--- second task siaode/binary_module.cpp
+++ second task siaode/binary_module.cpp
@@ -2,13 +2,15 @@
 #include <fstream>
 #include <time.h>
 #include <string>
+#include <iomanip>
 using namespace std;
-struct student
+struct business
 {
-    string fullName;
-    int groupNumber;
-    int studentNumber;
-    int key;
+    char license[8];
+    char name[20];
+    char founder[20];
+    bool isActive;
+
 };
 void create_bit(string name)
 {
@@ -22,117 +24,157 @@ void create_bit(string name)
     }
     while (in.good())
     {
-        string line;
-        getline(in, line);
-        string buf = "";
-        student st;
-        for (int i = 0; i < line.length(); i++)
-        {
-            if (line[i] != ' ')
-            {
-                buf += line[i];
-            }
-            else
-            {                   
-                fb.write((char*)&st, sizeof(student));
-                buf = "";
-            }
-        }
-        if (buf != "")
-        {           
-            fb.write((char*)&buf, sizeof(student));          
-        }
+        business st;
+        in >> st.license;
+        in >> st.name;
+        in >> st.founder;
+        string buf;
+        getline(in, buf);
+        if (buf == "1")
+            st.isActive = true;
+        else st.isActive = false;
+        in.get();
+        fb.write((char*)&st, sizeof(business));
     }
     fb.close();
     in.close();
 }
 void print_file(string name)
 {
-    ifstream fb(name + ".dat", ios::in | ios::binary);
+    ifstream fb(name + ".dat", ios::binary );
     if (!fb.good())
     {
         cout << "Can't open file!\n";
         return;
     }
+    business st;
+    fb.read((char*)&st, sizeof(business));
+    cout << "Name" << setw (23) << "founder" << setw(15) << "license" << setw(10) << "status\n";
     while (fb.good())
     {
-        int x;
-        fb.read((char*)&x, sizeof(student));
-        cout << x << " ";
-
+        cout << left << setw(20) << st.name << setw(15) << st.founder << setw(10) << st.license;
+        if (st.isActive)
+            cout << setw(10) << "active" << endl;
+        else cout << setw(10) << "not active" << endl;
+        fb.read((char*)&st, sizeof(business));
     }
     cout << endl;
     fb.close();
 }
-void text_to_binar(string textName,string name)
+bool text_to_binar(string textName,string name)
 {
-    ofstream fb(name + ".dat", ios::out | ios::binary);
+    ofstream fb;
+    fb.open(name + ".dat", ios::out | ios::binary);
     ifstream in(textName + ".txt", ios::in);
     if (!fb.good())
     {
         cout << "Can't open binary file!\n";
-        return;
+        return false;
     }
     if (!in.good())
     {
         cout << "Can't open text file!\n";
-        return;
+        return false;
     }
     while (in.good())
     {
-        string line;
-        getline(in, line);
-        string buf = "";
-        for (int i = 0; i < line.length(); i++)
-        {
-            if (line[i] != ' ')
-            {
-                buf += line[i];
-            }
-            else
-            {
-                try
-                {
-                    fb.write((char*)&buf, sizeof(student));
-                }
-                buf = "";
-            }
-        }
-        if (buf != "")
-        {
-            try
-            {
-                int num = stoi(buf);
-                fb.write((char*)&num, sizeof(student));
-            }
-            catch (invalid_argument const& ex)
-            {
-
-            }
-        }
+        business st;
+        in >> st.license;
+        in >> st.name;
+        in >> st.founder;
+        char buf;
+        in >> buf;
+        if (buf == '1')
+            st.isActive = true;
+        else st.isActive = false;
+        in.get();
+        fb.write((char*)&st, sizeof(business));
     }
     fb.close();
     in.close();
+    return true;
 }
-void binar_to_text(string textName, string name)
+bool binar_to_text(string textName, string name)
 {
-    ifstream fb(name + ".dat", ios::in | ios::binary);
+    ifstream fb;
+    fb.open(name + ".dat", ios::binary);
     ofstream txt(textName + ".txt", ios::out);
     if (!fb.good())
     {
         cout << "Can't open binary file!\n";
-        return;
+        return false;
     }
     if (!txt.good())
     {
         cout << "Can't open text file!\n";
-        return;
+        return false;
     }
+    business st;
+    fb.read((char*)&st, sizeof(business));
+    txt << st.license << "\n" << st.name << "\n" << st.founder << "\n";
+    if (st.isActive)
+        txt << "1";
+    else txt << "0";
+    fb.read((char*)&st, sizeof(business));
     while (fb.good())
     {
-        int x;
-        fb.read((char*)&x, sizeof(int));
-        
-
+        txt << st.license << "\n" << st.name << "\n" << st.founder << "\n";
+        if (st.isActive)
+            txt << "1";
+        else txt << "0";
+        fb.read((char*)&st, sizeof(business));       
+    }
+    return true;
+}
+business get_by_index(string name, int index) 
+{
+    ifstream fb;
+    business result;
+    fb.open(name + ".dat", ios::binary);
+    if (!fb.good())
+    {
+        cout << "Can't open binary file!\n";
+        result.name[0] = '0';
+        return result;
+    }
+    fb.seekg(index * sizeof(business));
+    fb.read((char*)&result, sizeof(business));
+    fb.close();
+    return result;
+}
+bool delete_by_key(string name, string key)
+{
+    fstream fb;
+    fb.open(name + ".dat", ios::out | ios::in | ios::binary);
+    if (!fb.good())
+    {
+        cout << "Can't open binary file!\n";
+        return false;
+    }
+    business lastElement;
+    business bufElement;
+    fb.read((char*)&lastElement, sizeof(business));
+    while (fb.good())
+        fb.read((char*)&lastElement, sizeof(business));
+    fb.close();
+    fb.open(name + ".dat", ios::out | ios::in | ios::binary);
+    fb.read((char*)&bufElement, sizeof(business));
+    while (fb.good() && bufElement.license != key)
+    {
+        fb.read((char*)&bufElement, sizeof(business));
+    }
+    fb.seekp(sizeof(bufElement) * (-1), ios::cur);
+    fb.write((char*)&lastElement, sizeof(business));
+    return true;
+}
+//personal
+void founder_list(string name, string founder)
+{
+    ifstream fb;
+    fb.open(name + ".dat", ios::out | ios::in | ios::binary);
+    if (!fb.good())
+    {
+        cout << "Can't open binary file!\n";
+        return;
     }
 }
